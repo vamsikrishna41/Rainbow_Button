500ddf0b79dbdc5189e56b03ad823db8e7cf4cb0
Bathula Vamsi Krishna <121428816+vamsikrishna41@users.noreply.github.com>
2023-07-20 10:10:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const color = ['violet','indigo','blue','green','yellow','orange','red'];
+body.style.backgroundColor='skyblue';
+document.addEventListener('click',function(){
+    const colorIndex=parseInt(Math.random()*color.length);
+    body.style.backgroundColor=color[colorIndex];
+});
